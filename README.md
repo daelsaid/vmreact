d0da34f877596d6fe7d4d94a3862171077cc499c
Dawlat El-Said <daelsaid@stanford.edu>
2018-07-12 14:29:03 -0700

    vmreact associated scripts
    vmreact workflows to administer task, organize output, grade the output, and create post-scoring measures

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vmreact
-these are the vmreact associated scripts for the inquisit task
+These directories represent the vmreact associated scripts that have been generated to parse and clean up the raw data output. They also generate additional measures.
+
+
+#vmreact_task_workflow
+
+* running this script will:
+
+1. Administer the task 
+2. Organize the participant data folder
+3. Convert .iqdat data to CSV
+4. Score and generate all relevant output
+5. log.txt the above process 
+
+
+* scripts:
+	* grader directory (with all needed scripts)
+	* full_inqquisit_wkflw_all_sites.sh
+		* see usage fxn
+* inquisit_task
+	* participant_dataa: organized and scored data output
+	* all_data: a copy of the raw .iqdat data
+	* task_script: script used for first timepoint
+	* task_scrpt_by_form: task scripts broken up by lists for second timepoint
+
+
+* New mexico example output can be found in participant_data 
---
+++ vmreact_task_workflow/scripts/grader/composite_scores.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 12:04:33 2018
+
+@author: dawlat_local
+"""
+
+import numpy as np
+import pandas
+import os
+from csv import reader,writer
+
+
+def composite_scores(get_comp_scores,input_csv,output_csv):
+    scored_data = pandas.read_csv(input_csv)
+
+    if get_comp_scores == 1:
+        df_trials=scored_data.loc[:,'subj_id':'trial7']
+        composite_scores=pandas.DataFrame()
+        tmp=pandas.DataFrame()
+
+        composite_scores[['subj_id','list_type']]=df_trials[['subj_id','list_type']]
+
+        composite_scores['total_learning']=df_trials[['trial1', 'trial2', 'trial3', 'trial4', 'trial5']].apply(lambda row: np.sum(row),axis=1)
+
+        tmp['test']=df_trials[['trial1']]*5
+
+        composite_scores['corrected_total_learning']=composite_scores['total_learning'].subtract(tmp['test'])
+
+        composite_scores['learning_rate']=df_trials['trial5'].subtract(df_trials['trial1'],axis='rows')
+        composite_scores['proactive_interference']=df_trials['trial1'].subtract(scored_data['listb'],axis='rows')
+        composite_scores['retroactive_interference']=df_trials['trial5'].subtract(df_trials['trial6'],axis='rows')
+        composite_scores['forgetting_and_retention']=df_trials['trial5'].subtract(df_trials['trial7'],axis='rows')
+        # composite_scores_transposed=composite_scores.transpose()
+        #composite_scores_transposed.to_csv(output_csv,header=True,index=['measure','score'])
+        composite_scores.to_csv(output_csv,header=True,index=['measure','score'])
